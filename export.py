f11775d5c118549f37a0c78f184edcc9e1310417
chraem <calumbiranc@gmail.com>
2021-09-01 23:39:31 +0800

    refactor: computation for x and y offsets

--- export.py
+++ export.py
@@ -7,9 +7,25 @@ from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment, Fill, Pat
 def generate_stub(company_name, employees, prepared_by):
 	print(employees)
 
-	for emp, empployee_data in enumerate(employees):
-              print(emp, empployee_data)
-              #0 (1, 'q', 'q', 'q', 1.0, 1.0, 1.0, 1.0, 1.0, 11.0, 1.0, 1.0, 1.0, 11.0, 1.0, 1.0, 14.0, '', 14.0)
+	for emp, employee_data in enumerate(employees):
+        # print(emp, employee_data)
+        # 0 (1, 'q', 'q', 'q', 1.0, 1.0, 1.0, 1.0, 1.0, 11.0, 1.0, 1.0, 1.0, 11.0, 1.0, 1.0, 14.0, '', 14.0)
+
+        # Size
+        col_per_stub = 11
+        row_per_stub = 22
+
+        # Offsets
+        x_offset = emp % 2 * col_per_stub + 1
+        y_offset = emp // 2 * row_per_stub + 1
+
+        #Simulation of X Offset Computation
+        # emp # %2 # *11 # + 1
+        #  0  # 0  #  0  # 1
+        #  1  # 1  #  11 # 12
+        #  2  # 0  #  0  # 1
+        #  3  # 1  #  11 # 12
+        #  4  # 0  #  0  # 1
 
 def save_wb(workbook, filename):
 	workbook.save(path.normpath(getcwd()+"/mugna/exports/"+filename))
